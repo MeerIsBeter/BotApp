60df2143761d6bc70f77ae856e05ea2f62c6616c
mdvmeijer <maxm@hotmail.nl>
2020-10-05 18:01:03 +0200

    converted onClick to onTouch for fancy buttons and added clear chat log functionality in the bot chat screen

--- app/src/main/java/com/example/chatbot/MainActivity.kt
+++ app/src/main/java/com/example/chatbot/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.chatbot
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 import kotlin.concurrent.thread
@@ -14,6 +17,7 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,55 +27,75 @@ class MainActivity : AppCompatActivity() {
         fun setBubbleAnimation(view: View) {
             val bubbleAnimation: Animation = AnimationUtils.loadAnimation(view.context, R.anim.grow)
 
-            val interpolator: BubbleInterpolator = BubbleInterpolator(0.1, 20.0)
+            val interpolator = BubbleInterpolator(0.1, 20.0)
             bubbleAnimation.interpolator = interpolator
 
             view.startAnimation(bubbleAnimation)
         }
 
         val botButton = findViewById<Button>(R.id.bot_button)
-        botButton.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(view: View): Unit {
-                setBubbleAnimation(botButton)
 
-                thread(start=true) {
-                    Thread.sleep(800)
+        botButton.setOnTouchListener(object: View.OnTouchListener {
+            var dX: Float = 0f
+            var dY: Float = 0f
+            var lastAction: Int = -10
 
-                    val intent = Intent(view.context, MessageListActivity::class.java)
-                    startActivity(intent)
+            override fun onTouch(view: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        dX = view.x - event.rawX
+                        dY = view.y - event.rawY
+                        lastAction = MotionEvent.ACTION_DOWN
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        view.y = event.rawY + dY
+                        view.x = event.rawX + dX
+                        lastAction = MotionEvent.ACTION_MOVE
+                    }
+                    MotionEvent.ACTION_UP -> if (lastAction == MotionEvent.ACTION_DOWN) {
+                        setBubbleAnimation(botButton)
+
+                        thread(start=true) {
+                            Thread.sleep(800)
+
+                            val intent = Intent(view.context, MessageListActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                    else -> return false
                 }
+                return true
             }
         })
 
         val helpButton = findViewById<Button>(R.id.help_button)
-        helpButton.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(view: View): Unit {
-                setBubbleAnimation(helpButton)
 
-//                thread(start=true) {
-//                    Thread.sleep(800)
-//
-//                    val intent = Intent(view.context, MessageListActivity::class.java)
-//                    startActivity(intent)
-//                }
+        helpButton.setOnTouchListener(object: View.OnTouchListener {
+            var dX: Float = 0f
+            var dY: Float = 0f
+            var lastAction: Int = -10
+
+            override fun onTouch(view: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        dX = view.x - event.rawX
+                        dY = view.y - event.rawY
+                        lastAction = MotionEvent.ACTION_DOWN
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        view.y = event.rawY + dY
+                        view.x = event.rawX + dX
+                        lastAction = MotionEvent.ACTION_MOVE
+                    }
+                    MotionEvent.ACTION_UP -> if (lastAction == MotionEvent.ACTION_DOWN) {
+                        setBubbleAnimation(helpButton)
+
+                        // TODO: make help page and redirect to it
+                    }
+                    else -> return false
+                }
+                return true
             }
         })
-
-
-//        button.setOnLongClickListener(object: View.OnLongClickListener {
-//            override fun onLongClick(view: View): Boolean {
-//                val shrinkAnimation: Animation = AnimationUtils.loadAnimation(view.context, R.anim.shrink)
-//
-//                val interpolator: BubbleInterpolator = BubbleInterpolator(0.05, 20.0)
-//                shrinkAnimation.interpolator = interpolator
-//
-//                button.startAnimation(shrinkAnimation)
-//                return true
-//
-////                val intent = Intent(view.context, MessageListActivity::class.java)
-////                startActivity(intent)
-//            }
-//        })
-
     }
 }
--- app/src/main/java/com/example/chatbot/MessageListActivity.kt
+++ app/src/main/java/com/example/chatbot/MessageListActivity.kt
@@ -9,17 +9,18 @@ import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.R.attr.start
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.DiffUtil
 import kotlin.concurrent.thread
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 
 
-
-
 class MessageListActivity : AppCompatActivity() {
 
-    val messageList : ArrayList<Message> = arrayListOf()
+    val messageList: ArrayList<Message> = arrayListOf()
+    val mMessageAdapter: MessageListAdapter = MessageListAdapter(this, messageList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +28,6 @@ class MessageListActivity : AppCompatActivity() {
 
         val mMessageRecycler = findViewById<RecyclerView>(R.id.recyclerview_message_list)
         mMessageRecycler.layoutManager = LinearLayoutManager(this)
-        val mMessageAdapter = MessageListAdapter(this, messageList)
         mMessageRecycler.adapter = mMessageAdapter
 
         val py = Python.getInstance()
@@ -37,58 +37,68 @@ class MessageListActivity : AppCompatActivity() {
             mMessageAdapter.notifyItemInserted(messageList.size-1)
         }
 
+        fun sendBotMessage(isResponding: Boolean, text: String) {
+            Thread.sleep(500)
+
+            val botText: String = if (isResponding) {
+                // call the Python chatbot module to construct an answer
+                mod.callAttr("main", text).toString()
+            } else {
+                // use whatever string was passed as argument
+                text
+            }
+
+            val isUser = false
+            val responseTime = System.currentTimeMillis()
+            val botMessage = Message(messageList.size-1, botText, isUser, responseTime)
+            messageList.add(botMessage)
+            runOnUiThread {
+                updateData()
+            }
+        }
+
+        sendBotMessage(false, "Hi there, welcome to this chatroom! I am a bot-in-training and I would love to talk with you, just send me a message whenever you want :)")
+
         // handle sending and receiving messages
         val chatBox = findViewById<EditText>(R.id.edittext_chatbox)
         val sendButton = findViewById<Button>(R.id.button_chatbox_send)
-        sendButton.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(view: View) {
-                // Handler code here.
-                val text = chatBox.text.toString()
-                if (text.isNotEmpty()) {
-                    // send user message
-                    val user = true
-                    val sendTime = System.currentTimeMillis()
-                    val message = Message(messageList.size-1, text, user, sendTime)
-                    messageList.add(message)
-                    updateData()
-                    chatBox.text.clear()
+        sendButton.setOnClickListener {
+            // Handler code here.
+            val text = chatBox.text.toString()
+            if (text.isNotEmpty()) {
+                // send user message
+                val user = true
+                val sendTime = System.currentTimeMillis()
+                val message = Message(messageList.size-1, text, user, sendTime)
+                messageList.add(message)
+                updateData()
+                chatBox.text.clear()
 
-                    thread(start=true) {
-                        Thread.sleep(500)
-                        // send bot message
-                        val botText = mod.callAttr("main", text).toString()
-                        val isUser = false
-                        val responseTime = System.currentTimeMillis()
-                        val botMessage = Message(messageList.size-1, botText, isUser, responseTime)
-                        messageList.add(botMessage)
-                        runOnUiThread {
-                            updateData()
-                        }
-                    }
-
-                    mMessageAdapter.notifyDataSetChanged()
-
+                thread(start=true) {
+                    sendBotMessage(true, text)
                 }
             }
-        })
+        }
     }
 
-    class DiffUtilCallback(
-        private val oldItems: List<Message>,
-        private val newItems: List<Message>
-    ) : DiffUtil.Callback() {
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldItems[oldItemPosition].id == newItems[newItemPosition].id
+    fun updateData() {
+        mMessageAdapter.notifyItemInserted(messageList.size-1)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu items for use in the action bar
+        val inflater = menuInflater
+        inflater.inflate(R.menu.action_bar_actions, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_clear_logs -> {
+                messageList.clear()
+                mMessageAdapter.notifyDataSetChanged()
+            }
         }
-
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            // It works properly if Item is a data class
-            // Otherwise, we should check if all fields of the items are the same
-            return oldItems[oldItemPosition] == newItems[newItemPosition]
-        }
-
-        override fun getOldListSize() = oldItems.size
-
-        override fun getNewListSize() = newItems.size
+        return true
     }
 }
