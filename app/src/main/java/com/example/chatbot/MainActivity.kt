914dce512299eaaba2e70c8e8597828225766083
mdvmeijer <maxm@hotmail.nl>
2020-10-05 22:56:16 +0200

    added robustness to the MainMenuTouchListener implementation

--- app/src/main/java/com/example/chatbot/MainActivity.kt
+++ app/src/main/java/com/example/chatbot/MainActivity.kt
@@ -3,15 +3,9 @@ package com.example.chatbot
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.view.View.OnTouchListener
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.dynamicanimation.animation.DynamicAnimation
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
 import com.chaquo.python.Python
@@ -29,20 +23,27 @@ class MainActivity : AppCompatActivity() {
         Python.start(AndroidPlatform(applicationContext))
 
         val botButton = findViewById<Button>(R.id.bot_button)
-
-        botButton.setOnTouchListener(MainMenuTouchListener {
-            thread(start = true) {
-                Thread.sleep(800)
-
-                val intent = Intent(botButton.context, MessageListActivity::class.java)
-                startActivity(intent)
-            }
-        })
-
         val helpButton = findViewById<Button>(R.id.help_button)
 
-        helpButton.setOnTouchListener(MainMenuTouchListener {
-            println("Nothing yet!")
-        })
+        // only create the touch listeners once the global layout has been loaded,
+        // to ensure the correct button coordinates are passed
+        val globalLayoutListener = OnGlobalLayoutListener {
+            botButton.setOnTouchListener(MainMenuTouchListener ( {
+                // start a new thread to start MessageListActivity
+                thread(start = true) {
+                    Thread.sleep(500)
+
+                    val intent = Intent(botButton.context, MessageListActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                    startActivity(intent)
+                }
+            }, botButton.x, botButton.y))
+
+            helpButton.setOnTouchListener(MainMenuTouchListener ( {
+                println("Nothing yet!")
+            }, helpButton.x, helpButton.y))
+        }
+
+        botButton.viewTreeObserver.addOnGlobalLayoutListener(globalLayoutListener)
     }
 }
--- app/src/main/java/com/example/chatbot/MainMenuTouchListener.kt
+++ app/src/main/java/com/example/chatbot/MainMenuTouchListener.kt
@@ -1,52 +1,59 @@
 package com.example.chatbot
 
-import android.content.Intent
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import androidx.core.content.ContextCompat.startActivity
 import androidx.dynamicanimation.animation.DynamicAnimation
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
-import kotlin.concurrent.thread
 
-class MainMenuTouchListener(function: () -> (Unit)) : OnTouchListener {
+
+class MainMenuTouchListener(function: () -> (Unit), var rootPosX: Float, var rootPosY: Float) : OnTouchListener {
     var dX = 0f
     var dY: Float = 0f
     var lastAction: Int = -10
-    var rootPosX = 0f
-    var rootPosY = 0f
     val clickAction: () -> Unit = function
+
+    private var animationX: SpringAnimation? = null
+    private var animationY: SpringAnimation? = null
 
     override fun onTouch(view: View, event: MotionEvent): Boolean {
         when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
+            MotionEvent.ACTION_DOWN -> { // the button is pressed
+                // compute the difference between the top-left of the view and where the event occurs
                 dX = view.x - event.rawX
                 dY = view.y - event.rawY
                 lastAction = MotionEvent.ACTION_DOWN
-                rootPosX = view.x
-                rootPosY = view.y
+                if (animationX != null && animationY != null) {
+                    animationX!!.cancel()
+                    animationY!!.cancel()
+                }
             }
-            MotionEvent.ACTION_MOVE -> {
+            MotionEvent.ACTION_MOVE -> { // the button is moved
                 view.y = event.rawY + dY
                 view.x = event.rawX + dX
                 lastAction = MotionEvent.ACTION_MOVE
             }
+            // perform clickAction() if the button was not moved between press and release
             MotionEvent.ACTION_UP -> if (lastAction == MotionEvent.ACTION_DOWN) {
                 doBubbleAnimation(view)
                 // perform the button-specific action
                 clickAction()
-            } else {
+                view.playSoundEffect(android.view.SoundEffectConstants.CLICK)
+            } else { // bounce back to its original position
                 doSpringAnimation(view, rootPosX, rootPosY)
+                animationX!!.start()
+                animationY!!.start()
             }
             else -> return false
         }
         return true
     }
 
-    fun doBubbleAnimation(view: View) {
+
+    private fun doBubbleAnimation(view: View) {
         val bubbleAnimation: Animation = AnimationUtils.loadAnimation(view.context, R.anim.grow)
 
         val interpolator = BubbleInterpolator(0.1, 20.0)
@@ -55,25 +62,21 @@ class MainMenuTouchListener(function: () -> (Unit)) : OnTouchListener {
         view.startAnimation(bubbleAnimation)
     }
 
-    fun doSpringAnimation(view: View, x: Float, y: Float) {
-        val animationX: SpringAnimation = SpringAnimation(view, DynamicAnimation.X)
+    private fun doSpringAnimation(view: View, x: Float, y: Float) {
+        animationX = SpringAnimation(view, DynamicAnimation.X)
 
         val springX: SpringForce = SpringForce()
         springX.finalPosition = x
         springX.stiffness = SpringForce.STIFFNESS_MEDIUM
         springX.dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
-        animationX.spring = springX
+        animationX!!.spring = springX
 
-
-        val animationY: SpringAnimation = SpringAnimation(view, DynamicAnimation.Y)
+        animationY = SpringAnimation(view, DynamicAnimation.Y)
 
         val springY: SpringForce = SpringForce()
         springY.finalPosition = y
         springY.stiffness = SpringForce.STIFFNESS_MEDIUM
         springY.dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
-        animationY.spring = springY
-
-        animationX.start()
-        animationY.start()
+        animationY!!.spring = springY
     }
 }
